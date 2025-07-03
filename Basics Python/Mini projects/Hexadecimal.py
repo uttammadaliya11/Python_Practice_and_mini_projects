f68cfc403b62d102e661f6233cb1f0e36938c894
Uttam Madaliya <uttammadaliya@gmail.com>
2025-07-03 15:08:44 +0200

    Update Hexadecimal.py

--- Basics Python/Mini projects/Hexadecimal.py
+++ Basics Python/Mini projects/Hexadecimal.py
@@ -7,12 +7,23 @@ hexNumbers = {
 # Converts a string hexadecimal number into an integer decimal
 # If hexNum is not a valid hexadecimal number, returns None
 def hexToDec(hexNum):
+
+    for char in hexNum:
+        if char not in hexNumbers:
+            return None
+    
+    if len(hexNum)==1:
+        result = ((hexNumbers[hexNum[0]])*(16**0))
+        return result
+    
     if len(hexNum)==2:
         result = (((16**1)*(hexNumbers[hexNum[0]]))+((hexNumbers[hexNum[1]])*(16**0)))
         return result
+    
     if len(hexNum)==3:
         result = (((16**2)*(hexNumbers[hexNum[0]]))+((16**1)*(hexNumbers[hexNum[1]]))+((hexNumbers[hexNum[2]])*(16**0)))
         return result
+    
     if len(hexNum)==4:
         result = (((16**3)*(hexNumbers[hexNum[0]]))+((16**2)*(hexNumbers[hexNum[1]]))+((16**1)*(hexNumbers[hexNum[2]]))+((hexNumbers[hexNum[3]])*(16**0)))
         return result
